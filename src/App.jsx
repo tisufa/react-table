abee2f4a92d41dc59e19fc0eaabbfdcd737de798
Titus Sutio Fanpula <tisufamoney@gmail.com>
2025-06-08 17:59:08 +0700

    2. Menyiapkan Workspace

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-screen h-screen grid py-10 justify-center bg-slate-200">
+      <div className="text-center ">
+        <h1 className="font-semibold text-3xl">React Table</h1>
+        <p className="mb-3">Create reusable react table from scratch</p>
+        <div className="bg-white shadow-md rounded-md w-[768px]">
+          <p className="p-3">Hello World</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
